1bf690ea9b14f06efe0ae65b9b87237f93a726b2
ytanii <ysft0729@gmail.com>
2025-10-01 10:30:23 -0400

    e

--- src/main/java/com/myorg/TodoappApp.java
+++ src/main/java/com/myorg/TodoappApp.java
@@ -3,36 +3,37 @@ package com.myorg;
 import software.amazon.awscdk.App;
 import software.amazon.awscdk.Environment;
 import software.amazon.awscdk.StackProps;
+import static java.util.Objects.requireNonNull;
 
 import java.util.Arrays;
 
 import dev.stratospheric.cdk.SpringBootApplicationStack;
 
 public class TodoappApp {
-    public static void main(final String[] args) {
-        App app = new App();
+        public static void main(final String[] args) {
+                App app = new App();
 
-        String accountId = (String) app
-                .getNode()
-                .tryGetContext("accountId");
-        requireNonNull(accountId, "context variable 'accountId' must not be null");
-        String region = (String) app
-                .getNode()
-                .tryGetContext("region");
-        requireNonNull(region, "context variable 'region' must not be null");
-        new SpringBootApplicationStack(
-                app,
-                "SpringBootApplication",
-                makeEnv(accountId, region),
-                "docker.io/ysft07/todo-app-v1:latest");
+                String accountId = (String) app
+                                .getNode()
+                                .tryGetContext("accountId");
+                requireNonNull(accountId, "context variable 'accountId' must not be null");
+                String region = (String) app
+                                .getNode()
+                                .tryGetContext("region");
+                requireNonNull(region, "context variable 'region' must not be null");
+                new SpringBootApplicationStack(
+                                app,
+                                "SpringBootApplication",
+                                makeEnv(accountId, region),
+                                "docker.io/ysft07/todo-app-v1:latest");
 
-        app.synth();
-    }
+                app.synth();
+        }
 
-    static Environment makeEnv(String account, String region) {
-        return Environment.builder()
-                .account(account)
-                .region(region)
-                .build();
-    }
+        static Environment makeEnv(String account, String region) {
+                return Environment.builder()
+                                .account(account)
+                                .region(region)
+                                .build();
+        }
 }
